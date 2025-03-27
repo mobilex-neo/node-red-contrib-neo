0cab3ad09351ba83bbbaca60eea94fbc616dc670
Luiz Carvalho <luizhmcarvalho@gmail.com>
2025-03-27 11:31:21 -0300

    Resolve bug on NeoCallMethod

--- nodes/neo-call-method/neo-call-method.js
+++ nodes/neo-call-method/neo-call-method.js
@@ -1,6 +1,6 @@
 // node-red-contrib-neo
 // Este módulo implementa nós para conectar ao NEO Framework no Node-RED
-
+const NeoClient = require('../../lib/neo-client');
 const fetch = require('node-fetch');
 
 module.exports = function (RED) {
@@ -12,20 +12,13 @@ module.exports = function (RED) {
 
         node.on('input', async function (msg) {
             try {
-                const session = msg.session || {};
-                const url = `${msg.uri || node.uri}/api/method/${node.methodName}`;
-                const options = {
-                    method: node.httpMethod,
-                    headers: session,
-                    body: node.httpMethod === 'POST' ? JSON.stringify(msg.payload) : null
-                };
-                const response = await fetch(url, options);
-                const data = await response.json();
-                msg.payload = data;
-            } catch (error) {
-                msg.payload = { error: error.message };
-            }
-            node.send(msg);
+                const client = new NeoClient(baseURL, token);
+                const result = await client.callMethod(node.httpMethod, node.httpMethod, msg.payload);
+                msg.payload = result;
+                node.send(msg);
+              } catch (err) {
+                node.error("Erro ao consultar documentos: " + err.message);
+              }
         });
     }
     RED.nodes.registerType("neo-call-method", NeoCallMethodNode);
